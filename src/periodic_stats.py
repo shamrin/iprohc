de0f9b0aadcbf3ff3951a12e40ab88968ceae9a3
Alexander Nikolaev <alexander.nikolaev@fairwaves.co>
2014-12-30 14:25:33 +0000

    send periodic stats to influx.db

--- src/periodic_stats.py
+++ src/periodic_stats.py
@@ -3,9 +3,6 @@
 Usage:
     python stats_server.py
 
-On another console:
-    while true; do sudo kill -USR1 $(cat /var/run/iprohc_server.pid); sleep 1; done
-
 To view InfluxDB data, go to its admin interface (on :8083), and query `rohc` DB
 with `select * from client.<address>`. E.g.: `select * from "client.172-30-1-175"`
 """
@@ -15,22 +12,19 @@ import os
 import socket
 import json
 import time
+import warnings
 from datetime import datetime
 
+import requests
+
 from cffi import FFI
-ffi = FFI()
 
-# load `struct udp_stats` definition
-ffi.cdef(open(os.path.dirname(os.path.abspath(__file__)) +
-              '/server/stats.h').read())
-
+USE_INFLUX = True # send stats to influx server at INFLUX_URL
+SHOW_STATS = True # dump stats to screen
+INFLUX_URL = "http://localhost:8086/db/rohc/series?u=root&p=root"
 UDP_IP = "127.0.0.1"
 UDP_PORT = 32032
-
-struct = ffi.new("struct udp_stats *")
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+HTTP_OK = 200
 
 COLUMNS = '''decomp_failed
              decomp_total
@@ -43,40 +37,58 @@ COLUMNS = '''decomp_failed
              unpack_failed
              total_received'''.split()
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+ffi = FFI()
+# load `struct udp_stats` definition
+ffi.cdef(open(os.path.dirname(os.path.abspath(__file__)) +
+              '/server/stats.h').read())
+struct = ffi.new("struct udp_stats *")
+
+
+def dump_to_screen(points, ip):
+  print "\n",
+  print '-'*13,
+  print datetime.now().strftime('%H:%M:%S'),
+  print '-'*15
+  i = 0
+  for col in COLUMNS:
+    print "%s:" % col,
+    print " "*(20 - len(col)),
+    print "%.2f" % (points[i]/delta_ts)
+    i += 1
+
+
+def save_to_influx(points, ip):
+  data = [dict(name = "client.%s" % ip.replace('.', '-'),
+               columns = COLUMNS, points = [points])]
+  r = requests.post(INFLUX_URL, json.dumps(data))
+  if r.status_code != HTTP_OK:
+    warings.warn(r.status_code, r.text)
+
+
 prev_stats = None
 prev_ts = datetime.now()
 
 while True:
-    nbytes, addr = sock.recvfrom_into(ffi.buffer(struct))
-    if nbytes == 0:
-        continue
+  nbytes, addr = sock.recvfrom_into(ffi.buffer(struct))
+  if nbytes == 0:
+    continue
 
-#    stats_packing = [struct.stats_packing[i]
-#                     for i in range(1, struct.n_stats_packing)]
+  stats = [getattr(struct, col) for col in COLUMNS]
+  cur_ts = datetime.now()
+  delta_ts = (cur_ts - prev_ts).total_seconds() 
 
-    stats = [getattr(struct, col) for col in COLUMNS]
-    cur_ts = datetime.now()
+  if prev_stats is not None:
+    points = [(now - prev)/delta_ts for prev, now in zip(prev_stats, stats)]
+    ip = socket.inet_ntoa(ffi.buffer(ffi.addressof(struct.client_addr)))
 
-    if prev_stats is None:
-        print ''
-        # print 'initial stats received (not sent):', dict(zip(COLUMNS, stats))
-    else:
-        
-        points = [(now - prev) for prev, now in zip(prev_stats, stats)]
-        ip = socket.inet_ntoa(ffi.buffer(ffi.addressof(struct.client_addr)))
+    if SHOW_STATS:
+      dump_to_screen(points, ip)
 
-        print "\n",
-        print '-'*13,
-        print datetime.now().strftime('%H:%M:%S'),
-        print '-'*15
-        i = 0
-        delta_ts = (cur_ts - prev_ts).total_seconds() 
-        for col in COLUMNS:
-            print "%s:" % col,
-            print " "*(20 - len(col)),
-            #print if col.endswith('') then "%.2f" % points[i]/delta_ts else points[i]
-            print "%.2f" % (points[i]/delta_ts)
-            i += 1
+    if USE_INFLUX:
+      save_to_influx(points, ip)
 
-    prev_stats = stats
-    prev_ts = cur_ts
+  prev_stats = stats
+  prev_ts = cur_ts
